60bac15c6933ddc455454cbee6494fab69295aca
ManiaDaneshfar <maniadaneshfar@outlook.com>
2021-05-02 23:48:38 +0430

    Add project files.

---
+++ Contacts/Model/Contacts.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Contacts.Model
+{
+    class Contacts
+    {
+        public int ContactID { get; set; }
+        public string Name { get; set; }
+        public string Family { get; set; }
+        public string Mobile { get; set; }
+        public string Email { get; set; }
+        public string Address { get; set; }
+
+    }
+}
